7f25cabc93121688fa0c95e83ca0ea65144cdab0
Simon Muscatello <s.muscatello@alpitronic.it>
2024-07-11 12:19:04 +0200

    new coords

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ class Bot:
     """
 
     def __init__(self):
-        self.team = "Alpitronic"  # This is your team name
+        self.team = "TeamName"  # This is your team name
         # This is the course that the ship has to follow
         self.course = [
             Checkpoint(latitude=46.51526380018685, longitude=-2.106954221077432, radius=3),
@@ -67,7 +67,31 @@ class Bot:
             Checkpoint(latitude=62.75645054920246, longitude=-167.6184920731297, radius=10),
             Checkpoint(latitude=48.69476863709392, longitude=-170.0381425172147, radius=10),
             Checkpoint(latitude=2.806318, longitude=-168.943864, radius=1990.0),
+            Checkpoint(latitude=2.783378485018821, longitude=120.2750675981632, radius=1),
+            Checkpoint(latitude=-4.371808511810304, longitude=116.3937656707797, radius=1),
+            Checkpoint(latitude=-4.629279687584559, longitude=114.4115097858007, radius=1),
+            Checkpoint(latitude=-5.764862726763404, longitude=106.0386820822321, radius=1),
+            Checkpoint(latitude=-6.11001139609985, longitude=105.7870113471664, radius=1),
+            Checkpoint(latitude=-6.29742096732878, longitude=105.5319416909656, radius=1),
+            Checkpoint(latitude=-6.114084860396257, longitude=104.6250388648793, radius=1),
+            Checkpoint(latitude=-6.022524480367786, longitude=80.03577043127072, radius=1),
+            Checkpoint(latitude=12.32364950720728, longitude=51.06186749020405, radius=1),
+            Checkpoint(latitude=11.68438658686913, longitude=43.91390326465574, radius=1),
+            Checkpoint(latitude=29.07795456671188, longitude=32.82494554815788, radius=1),
+            Checkpoint(latitude=29.89992425488417, longitude=32.62119154396041, radius=1),
+            Checkpoint(latitude=31.71593248722569, longitude=32.48269069721169, radius=1),
+            Checkpoint(latitude=36.95342443068156, longitude=12.50156611166445, radius=1),
+            Checkpoint(latitude=37.78784203471952, longitude=9.74117685531434, radius=1),
+            Checkpoint(latitude=35.75316212323058, longitude=-6.65594857105228, radius=1),
+            Checkpoint(latitude=37.07851166092445, longitude=-9.881346233229701, radius=1),
+            Checkpoint(latitude=43.41671723878878, longitude=-9.59315063982473, radius=1),
+            Checkpoint(
+                latitude=config.start.latitude,
+                longitude=config.start.longitude,
+                radius=1,
+            ),
         ]
+
 
     def run(
             self,
